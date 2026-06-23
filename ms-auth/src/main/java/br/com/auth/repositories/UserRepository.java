4eb4d040915041beeacd2772b595b6afa2b64f8b
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-06-23 15:46:08 -0300

    feat: Cria Repository de User

---
+++ ms-auth/src/main/java/br/com/auth/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.auth.repositories;
+
+import br.com.auth.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
