e284bca35a84f0ddc87307b224b0be2d776647f1
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-06-24 21:12:49 -0300

    build: Cria o projeto com JPA, Lombok, Spring Web e H2

---
+++ ms-courses/src/test/java/br/com/course/MsCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
