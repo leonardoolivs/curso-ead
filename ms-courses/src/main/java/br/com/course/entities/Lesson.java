e4fc740e21f2a548fe02a31d0055edde849c27a1
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-06-24 21:25:45 -0300

    feat: Cria entidade de Lesson

---
+++ ms-courses/src/main/java/br/com/course/entities/Lesson.java
@@ -0,0 +1,33 @@
+package br.com.course.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_LESSON")
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "video_url")
+    private String videoUrl;
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+}
